f89f816fb520aa4517a0bf8a7586fb4d6fe6984d
qiumouliang <qiumouliang@123.com>
2021-10-23 08:51:59 +0800

    first commit

---
+++ src/main/java/com/jzyh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.jzyh.git;
+
+/**
+ * @Description:
+ * @author:@wkg
+ * @Date:2021/10/23 8:47:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
